c18660a04ff50ebfcadde327ea38c375fa97bcfe
Xero kun <elo48858@gmail.com>
2023-07-29 10:43:14 +0100

    `FEAT` Added more string and vector examples

--- src/vectors.cpp
+++ src/vectors.cpp
@@ -15,5 +15,11 @@ int main()
         i++;
     }
 
+    string firstVectItem = string("The item at index 0 is ").append(watching.at(0));
+    cout << firstVectItem << endl;
+
+    string vectSize = string("The vector's size is ").append(to_string(watching.size()));
+    cout << vectSize << endl;
+
     return 0;
 }
